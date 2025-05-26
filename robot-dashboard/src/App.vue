b5480bcc66547750619817ec6108a2d6ebd8f62c
ydy87 <55203654+ydy87@users.noreply.github.com>
2025-05-26 16:48:30 +0900

    로봇 대시보드

---
+++ robot-dashboard/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Dashboard from './components/Dashboard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard
+  }
+}
+</script>
+
+<template>
+  <router-view />
+</template>
+
+
+<style>
+/* 기본 스타일 유지 */
+</style>
